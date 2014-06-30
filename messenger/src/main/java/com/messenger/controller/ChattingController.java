1a1ae25522216a4a8fa44ec7ae8a428c9f47dfc1
Hugh-Kim <nice5340@gmail.com>
2014-06-30 22:31:19 +0900

    테스트코드 추가 ModelAndView 사용방식 변경

--- messenger/src/main/java/com/messenger/controller/ChattingController.java
+++ messenger/src/main/java/com/messenger/controller/ChattingController.java
@@ -2,44 +2,43 @@ package com.messenger.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 import org.vertx.java.core.json.JsonObject;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
 /**
- *
+ * 
  * Created by Administrator on 2014-06-02.
  */
 @Controller
 public class ChattingController {
-    @Autowired
-    private ChatServerVerticle chatServerVerticle;
+	@Autowired
+	private ChatServerVerticle chatServerVerticle;
 
-    @RequestMapping({"/", "/index"})
-    public String chatRoomMain() {
-        return "index";
-    }
+	@RequestMapping({ "/", "/index" })
+	public String chatRoomMain() {
+		return "index";
+	}
 
-    @RequestMapping("/intoChatRoom")
-    public ModelAndView intoChatRoom(@RequestParam String userName) throws UnsupportedEncodingException {
-        ModelAndView modelAndView = new ModelAndView("/chatRoom");
-        modelAndView.addObject("userName", URLDecoder.decode(userName, "UTF-8"));
-        modelAndView.addObject("port", 1234);
-        return modelAndView;
-    }
+	@RequestMapping("/intoChatRoom")
+	public String intoChatRoom(@RequestParam String userName, Model model) throws UnsupportedEncodingException {
+		model.addAttribute("userName", URLDecoder.decode(userName, "UTF-8"));
+		model.addAttribute("port", 1234);
+		return "chatRoom";
+	}
 
-    @RequestMapping("/send")
-    @ResponseBody
-    public String send(@RequestParam final String userName, @RequestParam final String message) {
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.putString("userName", userName);
-        jsonObject.putString("message", message);
-        chatServerVerticle.getIo().sockets().emit("chatMessage", jsonObject);
-        return "ok";
-    }
+	@RequestMapping("/send")
+	@ResponseBody
+	public String send(@RequestParam final String userName, @RequestParam final String message) {
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.putString("userName", userName);
+		jsonObject.putString("message", message);
+		chatServerVerticle.getIo().sockets().emit("chatMessage", jsonObject);
+		return "ok";
+	}
 }
--- messenger/src/test/java/com/messenger/controller/ChattingControllerTest.java
+++ messenger/src/test/java/com/messenger/controller/ChattingControllerTest.java
@@ -1,22 +1,65 @@
 package com.messenger.controller;
 
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:dispatcher-servlet.xml")
 public class ChattingControllerTest {
-    private ModelAndView modelAndView;
+
+    private MockHttpServletRequest mockHttpServletRequest;
+    private MockHttpServletResponse mockHttpServletResponse;
+    private HandlerAdapter handlerAdapter;
+    private ChattingController chattingController;
+
+    @Autowired
+    private ApplicationContext context;
 
     @Before
     public void setUp() throws Exception {
-        modelAndView = new ModelAndView();
+        mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletResponse = new MockHttpServletResponse();
+        handlerAdapter = new AnnotationMethodHandlerAdapter();
+//        chattingController = new ChattingController();
+        chattingController = context.getBean(ChattingController.class);
     }
 
     @Test
     public void testMakeChatRoom() throws Exception {
+        mockHttpServletRequest.setRequestURI("/index");
+        final ModelAndView mav = handlerAdapter.handle(mockHttpServletRequest, mockHttpServletResponse, chattingController);
+        ModelAndViewAssert.assertViewName(mav, "index");
+    }
 
+    @Test
+    public void testIntoChatRoom() throws Exception {
+        mockHttpServletRequest.setRequestURI("/intoChatRoom");
+        mockHttpServletRequest.setParameter("userName", "testUser");
+        final ModelAndView mav = handlerAdapter.handle(mockHttpServletRequest, mockHttpServletResponse, chattingController);
+        ModelAndViewAssert.assertViewName(mav, "chatRoom");
+        ModelAndViewAssert.assertModelAttributeValue(mav, "userName", "testUser");
+        ModelAndViewAssert.assertModelAttributeValue(mav, "port", 1234);
+    }
+
+    @Test
+    public void testSend() throws Exception {
+        mockHttpServletRequest.setRequestURI("/send");
+        mockHttpServletRequest.setParameter("userName", "testUser");
+        mockHttpServletRequest.setParameter("message", "testMessage");
+        handlerAdapter.handle(mockHttpServletRequest, mockHttpServletResponse, chattingController);
+        Assert.assertThat(mockHttpServletResponse.getContentAsString(), Matchers.is("ok"));
     }
 }
